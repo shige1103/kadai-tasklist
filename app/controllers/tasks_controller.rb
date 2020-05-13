c57e70820663039446d21e2fc39179b9bebdea7b
shigenori miyazaki <s1i1g0e3@gmail.com>
2020-05-13 09:05:20 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,4 +46,11 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
+  
+  private
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
