4e6811a60a3d00915b155b2f5ab29c382ea7ed3f
shigenori miyazaki <s1i1g0e3@gmail.com>
2020-05-27 14:22:41 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render "tasks/index"
+      render :new
     end
   end
 
